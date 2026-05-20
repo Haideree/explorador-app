949747aea918ff10d0d7ca01e9c1f4ee5988b61f
Haider <hfuentes3@udi.edi.co>
2026-05-20 10:38:16 -0500

    Add React description component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Navbar from "./components/Navbar";
 import Countries from "./components/Countries";
 import Weather from "./components/Weather";
 import AIRecommendation from "./components/AIRecommendation";
+import ReactInfo from "./components/ReactInfo";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ReactInfo />
       <Countries />
       <Weather />
       <AIRecommendation />
---
+++ src/components/ReactInfo.jsx
@@ -0,0 +1,28 @@
+function ReactInfo() {
+
+  return (
+
+    <div
+      style={{
+        background: "white",
+        padding: "15px",
+        borderRadius: "10px",
+        marginBottom: "20px"
+      }}
+    >
+
+      <h2>¿Qué es React?</h2>
+
+      <p>
+        React es una librería de JavaScript creada por Meta
+        que permite construir interfaces de usuario mediante
+        componentes reutilizables y dinámicos.
+      </p>
+
+    </div>
+
+  );
+
+}
+
+export default ReactInfo;
